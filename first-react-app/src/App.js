6e30e0dfd46528534a62d86f9bb966537c525802
bestlalala <bestdustn@gmail.com>
2022-04-13 17:25:16 +0900

    useEffect 함수 사용하기

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,11 +1,36 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev +1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+    if (keyword !== "" && keyword.length > 5) { //keyword의 길이가 5보다 클 때 검색하겠다.
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]); // keyword가 변할 때만 실행
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]); // counter가 변할 때만 실행
+  useEffect(() => {
+    console.log("I run when keyword & counter change")
+  }, [keyword, counter]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"}/>
+      <input 
+      value={keyword} 
+      onChange={onChange} 
+      type="text" p
+      laceholder="Search here..."/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
