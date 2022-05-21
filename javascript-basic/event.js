90d0a3f82898dae6737564d4a8736acf17d13ae4
yeonsu <bestdustn@sookmyung.ac.kr>
2022-05-22 00:03:45 +0900

    폴더 정리

--- javascript-basic/event.js
+++
@@ -1,7 +0,0 @@
-// querySelector 이용 -> 원하는 요소 가져오기!
-const content = document.querySelector("div.content h1");
-
-function handleContentClick() {
-    content.style.color = "orange";
-}
-content.addEventListener("click", handleContentClick);
