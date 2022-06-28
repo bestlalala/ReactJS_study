d05e2f05559a4bce823ef77a673b9273149db46c
yeonsu <bestdustn@sookmyung.ac.kr>
2022-06-28 20:31:00 +0900

    프론트 4주차 세션

---
+++ likelion-seminar/reactstudy2/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+function App() {
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+        setToDos((currentArray) => [toDo, ...currentArray]);
+        setToDo("");
+    }
+    return (
+        <div>
+            <h1>My To Do List [{toDos.length}개]</h1>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    value={toDo}
+                    type="text"
+                    placeholder="Write your to do ..."
+                />
+                <button> ADD </button>
+            </form>
+            <hr />
+            {/* 컴포넌트를 리턴해주기 */}
+            <ul>
+                {toDos.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
